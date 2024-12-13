1b23c67c1e97a1c3b32495bfc4f0e25db073e4b2
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-13 09:11:12 +0100

    first commit

---
+++ src/microservices.model.ts
@@ -0,0 +1,51 @@
+import { Module } from '@nestjs/common';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+    providers: [
+        {
+            provide: 'SERVICE_1_auth',
+            useFactory: () => {
+                return ClientProxyFactory.create({
+                    transport: Transport.TCP,
+                    options: {
+                        host: process.env.SERVICE_1_HOST || 'localhost',
+                        port: parseInt(process.env.SERVICE_1_PORT, 10) || 3000,
+                    },
+                });
+            },
+        },
+        {
+            provide: 'SERVICE_2_RH',
+            useFactory: () => {
+                return ClientProxyFactory.create({
+                    transport: Transport.TCP,
+                    options: {
+                        host: process.env.SERVICE_2_HOST || 'localhost',
+                        port: parseInt(process.env.SERVICE_2_PORT, 10) || 4000,
+                    },
+                });
+            },
+        },
+        {
+            provide: 'SERVICE_3_employee',
+            useFactory: () => {
+                return ClientProxyFactory.create({
+                    transport: Transport.TCP,
+                    options: {
+                        host: process.env.SERVICE_3_HOST || 'localhost',
+                        port: parseInt(process.env.SERVICE_3_PORT, 10) || 5000,
+                    },
+                });
+            },
+        },
+    ],
+
+    exports: [
+        'SERVICE_1_auth',
+        'SERVICE_3_employee',
+        'SERVICE_2_RH'
+    ]
+})
+export class MicroservicesModule {}

abaee4517acb52a63d05ffd18d8a81b98bec7ee3
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-19 09:20:30 +0100

    refactor: mise à jour des modules dans app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MicroservicesModule } from './microservices.model';
+import { GatewayController } from './gateway.controller';
+import { EmployeesModule } from './employee/employee.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    MicroservicesModule,
+    EmployeesModule
+  ],
+  controllers: [AppController, GatewayController],
   providers: [AppService],
 })
 export class AppModule {}

0790a5e545c300ffe917408958efc7ca78a059df
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-19 09:20:33 +0100

    chore: ajustements dans le point d'entrée principal

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8000);
+
+
+  app.connectMicroservice({
+    
+    transport: Transport.TCP ,
+    options: {
+      host: 'localhost',
+      port: 6379,
+    },
+      
+  });
+
+  await app.startAllMicroservices();
+
+  await app.listen(6379);
 }
 bootstrap();
